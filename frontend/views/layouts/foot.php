1bdec86808043d391ba2cf0433f573093e33ed2f
GeorgeFreeman <thenewsit@gmail.com>
2016-01-26 11:44:12 +0500

    minify js

--- frontend/views/layouts/foot.php
+++ frontend/views/layouts/foot.php
@@ -5,14 +5,16 @@
 	<script src="/js/libs/respond/respond.min.js"></script>
 	<![endif]-->
 
-	<?php $this->registerJsFile('/js/libs/bootstrap/js/bootstrap.js', ['position' => yii\web\View::POS_END ]);?>
+	<?php $this->registerJsFile('/js/modernizr.min.js', ['position' => yii\web\View::POS_END ]);?>
+
+	<?php $this->registerJsFile('/js/libs/bootstrap/js/bootstrap.min.js', ['position' => yii\web\View::POS_END ]);?>
 	<?php $this->registerJsFile('/js/libs/waypoints/waypoints.min.js', ['position' => yii\web\View::POS_END ]);?>
 	<?php $this->registerJsFile('/js/libs/magnific/jquery.magnific-popup.min.js', ['position' => yii\web\View::POS_END ]);?>
-	<?php $this->registerJsFile('/js/libs/bxslider/jquery.bxslider.js', ['position' => yii\web\View::POS_END ]);?>
+	<?php $this->registerJsFile('/js/libs/bxslider/jquery.bxslider.min.js', ['position' => yii\web\View::POS_END ]);?>
 	<?php $this->registerJsFile('/js/jquery.parallax.min.js', ['position' => yii\web\View::POS_END ]);?>
 	<?php $this->registerJsFile('/js/parallax.min.js', ['position' => yii\web\View::POS_END ]);?>
 	<?php $this->registerJsFile('/js/wow.min.js', ['position' => yii\web\View::POS_END ]);?>
 	<?php $this->registerJsFile('/js/jquery-validate.js', ['position' => yii\web\View::POS_END ]);?>
-	<?php $this->registerJsFile('/js/jquery.mb.YTPlayer.js', ['position' => yii\web\View::POS_END ]);?>
+	<?php $this->registerJsFile('/js/jquery.mb.YTPlayer.min.js', ['position' => yii\web\View::POS_END ]);?>
 	<?php $this->registerJsFile('/js/jquery.PageScroll2id.min.js', ['position' => yii\web\View::POS_END ]);?>
 	<?php $this->registerJsFile('/js/common.js', ['position' => yii\web\View::POS_END ]);?>
