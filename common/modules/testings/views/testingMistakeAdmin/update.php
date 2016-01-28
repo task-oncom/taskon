dff3f84d6cf70c33d784538e46651b67df715924
GeorgeFreeman <thenewsit@gmail.com>
2016-01-28 15:12:16 +0500

    fix

--- common/modules/testings/views/testingMistakeAdmin/update.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$this->tabs = array(
-    //'управление' => $this->createUrl('manage'),
-    'просмотр'   => $this->createUrl('view', array('passing' => $form->model->passing_id))
-);
-
-echo $form;
